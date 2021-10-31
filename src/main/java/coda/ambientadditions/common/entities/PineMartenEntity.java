e300342c5fb49519ca0ee8b7fd9bb910e34eea25
Coda <cody.koelliker@outlook.com>
2021-10-30 20:32:33 -0600

    fix pine marten tempt item

--- src/main/java/coda/ambientadditions/common/entities/PineMartenEntity.java
+++ src/main/java/coda/ambientadditions/common/entities/PineMartenEntity.java
@@ -33,7 +33,7 @@ public class PineMartenEntity extends AnimalEntity {
         this.goalSelector.addGoal(0, new SwimGoal(this));
         this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
         this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
-        this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.of(Items.POTATO), false));
+        this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.of(Items.EGG), false));
         this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.25D));
         this.goalSelector.addGoal(5, new LookAtGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
