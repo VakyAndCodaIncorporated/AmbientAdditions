fd3d154efbcbfb68634568fe6a03c8522dd855e7
Coda <cody.koelliker@outlook.com>
2022-03-14 22:59:19 -0600

    remove unused imports

--- src/main/java/coda/ambientadditions/common/items/YetiArmWarmersItem.java
+++ src/main/java/coda/ambientadditions/common/items/YetiArmWarmersItem.java
@@ -6,7 +6,6 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
