b83214ffad356818d704c7851838f17e324de6ad
Cody <cody.koelliker@outlook.com>
2022-11-11 22:08:16 -0700

    bit more work

--- src/main/java/coda/ambientadditions/common/entities/BluntheadTreeSnakeEntity.java
+++ src/main/java/coda/ambientadditions/common/entities/BluntheadTreeSnakeEntity.java
@@ -16,8 +16,6 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.animal.Chicken;
-import net.minecraft.world.entity.animal.Parrot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -72,13 +70,12 @@ public class BluntheadTreeSnakeEntity extends Animal implements IAnimatable {
         this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.15D, true));
         this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Player.class, 4.0F, 1.15D, 1.35D));
         this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
-        this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.of(Items.CHICKEN), false));
+        this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.of(AAItems.PINOCCHIO_ANOLE_POT.get()), false));
         this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.25D));
         this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 6.0F));
         this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D, 5));
         this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Chicken.class, false));
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Parrot.class, false));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, PinocchioAnoleEntity.class, false));
     }
 
     @Override
