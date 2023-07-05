8a3f22d8aa2207dca41fe54906a7f9a6afc078e6
Coda <cody.koelliker@outlook.com>
2023-07-05 12:40:47 -0600

    add ducky mask tooltip

--- src/main/java/codyhuh/ambientadditions/common/items/DuckyMaskItem.java
+++ src/main/java/codyhuh/ambientadditions/common/items/DuckyMaskItem.java
@@ -2,18 +2,22 @@ package codyhuh.ambientadditions.common.items;
 
 import codyhuh.ambientadditions.AmbientAdditions;
 import codyhuh.ambientadditions.registry.AAItems;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
@@ -40,6 +44,26 @@ public class DuckyMaskItem extends GeoArmorItem implements IAnimatable {
             }
         }
     }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        Minecraft mc = Minecraft.getInstance();
+
+        MutableComponent info = Component.empty();
+        info.append(Component.translatable("tooltip.ambientadditions.shift_for_info1").withStyle(ChatFormatting.DARK_GRAY));
+        info.append(mc.options.keyShift.getTranslatedKeyMessage()).withStyle(ChatFormatting.GRAY);
+        info.append(Component.translatable("tooltip.ambientadditions.shift_for_info2").withStyle(ChatFormatting.DARK_GRAY));
+
+        Component extraInfo = Component.translatable("tooltip.ambientadditions.ducky_mask").withStyle(Style.EMPTY.withColor(0xe4b73a));
+
+        if (Screen.hasShiftDown()) {
+            components.add(extraInfo);
+        }
+        else {
+            components.add(info);
+        }
+    }
+
 
     @Override
     public void registerControllers(AnimationData animationData) {
