a4738dedbce0e0ca1d6ac05837c272b05d380b8b
Cody <cody.koelliker@outlook.com>
2024-01-04 16:23:29 -0700

    remove mixins

--- src/main/java/codyhuh/ambientadditions/mixin/AnimalMixin.java
+++ src/main/java/codyhuh/ambientadditions/mixin/AnimalMixin.java
@@ -8,23 +8,19 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
-import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Animal.class)
+//@Mixin(Animal.class)
 public class AnimalMixin extends PathfinderMob {
 
     protected AnimalMixin(EntityType<? extends PathfinderMob> type, Level level) {
         super(type, level);
     }
 
-    @Inject(method = "mobInteract(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult;", at = @At("HEAD"), cancellable = true)
+    //@Inject(method = "mobInteract(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult;", at = @At("HEAD"), cancellable = true)
     public void ambientadditions_mobInteract(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
         var cap = getCapability(SedationProvider.SEDATION_CAP);
 
